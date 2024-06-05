ec003685fd9b87701d613dd9ed65122acc41f6bc
konstantin <konst.gromov@gmail.com>
2024-06-06 01:26:38 +0300

    Add image recognition endpont

---
+++ src/main/java/guru/springframework/springaiimage/controllers/ImageModelController.java
@@ -0,0 +1,32 @@
+package guru.springframework.springaiimage.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Media;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageModelController {
+    private final ChatModel chatModel;
+
+    @GetMapping("/describe")
+    public String describe() {
+        var imageResource = new ClassPathResource("/images/drugs.jpg");
+        UserMessage userMessage = new UserMessage("""
+                The following is a screenshot of drugs. 
+                Can you do your best to provide a name and expiration date?
+                Answer in format: name - date(MM/yyyy)
+                """,
+                List.of(new Media(MimeTypeUtils.IMAGE_JPEG, imageResource)));
+        var response = chatModel.call(new Prompt(userMessage));
+        return response.getResult().getOutput().getContent();
+    }
+}
--- src/main/java/guru/springframework/springaiimage/services/OpenAIService.java
+++ src/main/java/guru/springframework/springaiimage/services/OpenAIService.java
@@ -1,10 +1,40 @@
 package guru.springframework.springaiimage.services;
 
-
 import guru.springframework.springaiimage.model.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.image.ImageOptionsBuilder;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.stereotype.Service;
 
+import java.util.Base64;
 
-public interface OpenAIService {
+import static org.springframework.ai.openai.api.OpenAiImageApi.ImageModel.DALL_E_3;
 
-    byte[] getImage(Question question);
+@RequiredArgsConstructor
+@Service
+public class OpenAIService {
+
+    private final OpenAiImageModel imageModel;
+
+    public byte[] getImage(Question question) {
+        var extendedOptions = OpenAiImageOptions.builder()
+                .withHeight(1024).withWidth(1792)
+                .withResponseFormat("b64_json")
+                .withModel(DALL_E_3.getValue())
+                .withQuality("hd") //default standard
+                .withStyle("natural") //default vivid
+                .build();
+
+        var options = ImageOptionsBuilder.builder()
+                .withHeight(1024).withWidth(1024)
+                .withModel(DALL_E_3.getValue())
+                .withResponseFormat("b64_json")
+                .build();
+
+        ImagePrompt imagePrompt = new ImagePrompt(question.question(), options);
+        var imageResponse = imageModel.call(imagePrompt);
+        return Base64.getDecoder().decode(imageResponse.getResult().getOutput().getB64Json());
+    }
 }
