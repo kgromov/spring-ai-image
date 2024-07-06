bba0cd6f915318e426e6303c66e59230078feda0
konstantin <konst.gromov@gmail.com>
2024-07-07 02:10:52 +0300

    Add image to code endpoint

--- src/main/java/guru/springframework/springaiimage/controllers/ImageModelController.java
+++ src/main/java/guru/springframework/springaiimage/controllers/ImageModelController.java
@@ -1,6 +1,7 @@
 package guru.springframework.springaiimage.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Media;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
@@ -8,6 +9,7 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,5 +30,17 @@ public class ImageModelController {
                 List.of(new Media(MimeTypeUtils.IMAGE_JPEG, imageResource)));
         var response = chatModel.call(new Prompt(userMessage));
         return response.getResult().getOutput().getContent();
+    }
+
+    @GetMapping("/image-to-code")
+    public String imageToCode(@RequestParam String image) {
+        var imageResource = new ClassPathResource("/screens/%s.png".formatted(image));
+        return ChatClient.builder(chatModel)
+                .build()
+                .prompt()
+                .system("The following is a screenshot of some code. Can you translate this from the image into text?")
+                .user(um -> um.media(MimeTypeUtils.IMAGE_PNG, imageResource))
+                .call()
+                .content();
     }
 }
