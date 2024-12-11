f0288b2fdfacf4a194356676e149942afdb47b99
konstantin <konst.gromov@gmail.com>
2024-12-12 00:11:36 +0200

    Ollama vision model really sucks (neither with new milestone)

--- src/main/java/guru/springframework/springaiimage/controllers/ImageModelController.java
+++ src/main/java/guru/springframework/springaiimage/controllers/ImageModelController.java
@@ -2,10 +2,10 @@ package guru.springframework.springaiimage.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.Media;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.model.Media;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
