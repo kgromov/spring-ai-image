1e384ad7c841e32e178e23d40611886612f072a5
konstantin <konst.gromov@gmail.com>
2025-02-06 01:50:29 +0200

    Update to M4

--- src/main/java/guru/springframework/springaiimage/controllers/ImageModelController.java
+++ src/main/java/guru/springframework/springaiimage/controllers/ImageModelController.java
@@ -2,7 +2,7 @@ package guru.springframework.springaiimage.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.Media;
+import org.springframework.ai.model.Media;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
